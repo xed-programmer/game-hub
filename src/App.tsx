c3a7a7b4a61780f816a437d1eaf66104de1b3655
Dexter Aragon <xedprogrammer@gmail.com>
2024-04-11 15:20:00 +0800

    showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible,setAlertVisible] = useState(false);
   return (
-  <>
-    <Button color="danger" onClick={()=>console.log('Clicked')}>
-      Click Me
-    </Button>
-  </>
+    <>
+      {alertVisible===true && <Alert onClose={()=> setAlertVisible(false)}>My ALert</Alert>}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>
+        clicked
+      </Button>
+    </>
   )
 }
 
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,10 +2,15 @@ import { ReactNode } from "react";
 
 interface Props {
     children: ReactNode;
+    color?: string;
+    onClose: () => void;
 }
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, color = 'primary', onClose }: Props) => {
     return (
-        <div className="alert alert-primary">{children}</div>
+        <div className={"alert alert-"+color+" alert-dismissible fade show"}>
+            {children}
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}></button>
+        </div>
     )
 }
 
